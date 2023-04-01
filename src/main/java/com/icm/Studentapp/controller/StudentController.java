b3834ef533153e349a955ccc6616ddef7260365b
Dilan-Lakshitha <lakshithadilan6@gmail.com>
2023-04-01 11:55:24 +0530

    data to connect to database

--- src/main/java/com/icm/Studentapp/controller/StudentController.java
+++ src/main/java/com/icm/Studentapp/controller/StudentController.java
@@ -1,19 +1,28 @@
 package com.icm.Studentapp.controller;
 
+import com.icm.Studentapp.dto.request.RequestStudentDto;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/students")
 public class StudentController {
-    public String saveStudent(){
-        return "save student";
+    @PostMapping("/create")
+    public String saveStudent(@RequestBody RequestStudentDto dto){return dto.toString();
     }
-    public String findStudent(){
+    @GetMapping("/{id}")
+    public String findStudent(@PathVariable String id){
         return "find student";
     }
-    public String deleteStudent(){
+    @DeleteMapping(params = "id")
+    public String deleteStudent(@RequestParam String id){
         return "delete student";
-    }
-    public String updateStudent(){
+    }//
+    @PutMapping
+    public String updateStudent(@RequestBody RequestStudentDto dto,@RequestParam String id){
         return "update student";
     }
-    public String findAllStudent(){
+    @GetMapping(path = "/list", params = {"page","size"})
+    public String findAllStudent(@RequestParam int page,int size){
         return "find student";
     }
 }
