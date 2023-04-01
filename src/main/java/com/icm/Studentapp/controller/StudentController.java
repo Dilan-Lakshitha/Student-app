d6c2bc45c90b7f79339e961f186d08164b47c4be
Dilan-Lakshitha <lakshithadilan6@gmail.com>
2023-04-01 10:16:17 +0530

    Springboot

---
+++ src/main/java/com/icm/Studentapp/controller/StudentController.java
@@ -0,0 +1,19 @@
+package com.icm.Studentapp.controller;
+
+public class StudentController {
+    public String saveStudent(){
+        return "save student";
+    }
+    public String findStudent(){
+        return "find student";
+    }
+    public String deleteStudent(){
+        return "delete student";
+    }
+    public String updateStudent(){
+        return "update student";
+    }
+    public String findAllStudent(){
+        return "find student";
+    }
+}
